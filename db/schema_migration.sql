3d75c3afd3742eacae3d7df8e9442bd8b0add9bf
Jane Justiz <janejustiz@gmail.com>
2019-08-05 17:58:23 +0000

    Automatically backed up by Learn

--- db/schema_migration.sql
+++ db/schema_migration.sql
@@ -1,9 +1,6 @@
 CREATE TABLE pokemon(
   id INTEGER PRIMARY KEY,
   name TEXT,
-  type TEXT
-<<<<<<< HEAD
+  type TEXT,
+  hp INTEGER
 );
-=======
-)
->>>>>>> f16f96e05644eecab46ff17036084fcbfb1e3d24
